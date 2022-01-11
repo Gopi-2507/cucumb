7dd88e6287ff6230fb9402461a8e0423546d25d7
Gopi-2507 <gopinath.hic@gmail.com>
2022-01-11 17:09:48 +0530

    new Code

---
+++ src/org/git/test/SampleGit.java
@@ -0,0 +1,12 @@
+package org.git.test;
+
+public class SampleGit {
+	private void hoe() {
+		System.out.println("employee name");
+
+	}
+	private void hlo() {
+		System.out.println("emlyoees contact");
+
+	}
+}
